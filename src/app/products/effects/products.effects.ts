91ae3b6e53e11d2a185d9f5f818ad5c6f75471d7
unknown <https://avivronel@bitbucket.org>
2020-08-03 15:51:07 +0300

    fixed some stuff before sakak

--- src/app/products/effects/products.effects.ts
+++ src/app/products/effects/products.effects.ts
@@ -2,7 +2,7 @@ import { EMPTY } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap, catchError, tap } from 'rxjs/operators';
+import { map, mergeMap, catchError } from 'rxjs/operators';
 
 import { loadProducts, productsLoaded } from '../actions/products.actions';
 import { Product } from '../../models/product.model';
@@ -13,13 +13,13 @@ export class ProductsEffects {
   private readonly jsonPath = '/assets/json/products.json';
   
   //i think http.get should be in a service but i was told to delete my services
-  loadProducts$ = createEffect(() => this.actions$.pipe(tap(console.log),
+  loadProducts$ = createEffect(() => this.actions$.pipe(
     ofType(loadProducts),
-    mergeMap(() => {console.log("fuckk");return this.http.get<Product[]>(this.jsonPath)
+    mergeMap(() => this.http.get<Product[]>(this.jsonPath)
       .pipe(
         map(products => {console.log("products",products);return productsLoaded({products: products})}),
         catchError(() => EMPTY)
-      )}), tap(console.log)
+      ))
     )
   );
  
